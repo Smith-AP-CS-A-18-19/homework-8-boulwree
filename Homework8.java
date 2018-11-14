372aeafe1801c19444f6c752822c41160209b191
Reece Boulware <boulwree@berkeleyprep.org>
2018-11-14 13:54:12 -0500

    how r u?

--- Homework8.java
+++ Homework8.java
@@ -1,22 +1,27 @@
+//Reece Boulware
 public class Homework8 {
 
 	/* Finish the constructor and create any necessary instance
 	 * variables. A Homework8 object should have an array that
 	 * holds n int values
 	 */
+	 private int[] arr;
 	public Homework8(int n) {
-
+		 this.arr = new int[n];
 	}
 
 	/* Return the stored array
 	 */
 	public int[] problem1() {
-
+		return arr;
 	}
 
 	/* Fill the stored array with the provided int
 	 */
 	public void problem2(int n) {
+		for (int i = 0; i < arr.length; i++){
+			arr[i] = n;
+		}
 
 	}
 
@@ -24,6 +29,10 @@ public class Homework8 {
 	 * n, the second n-1, the third n-2, etc.
 	 */
 	public void problem3(int n) {
+		for (int i = 0; i < arr.length; i++){
+			arr[i] = n;
+			n = n - 1;
+		}
 
 	}
 
@@ -31,6 +40,13 @@ public class Homework8 {
 	 * provided array
 	 */
 	public double problem4(double[] arr) {
+		double largest = arr[0];
+		for (int i = 1; i < arr.length; i++){
+			if(arr[i] > largest){
+				largest = arr[i];
+			}
+		}
+		return largest;
 
 	}
 
@@ -39,8 +55,15 @@ public class Homework8 {
 	 * is not in the array
 	 */
 	public int problem5(int[] arr, int num) {
-
-	}
+		for (int i : arr){
+			if(num == arr[i]){
+				return num;
+			} else{
+				return -1;
+			}
+		}
+		return num;
+}
 
 	public static void main(String[] args) {
 		boolean pass = true;
